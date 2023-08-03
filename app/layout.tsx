ee41734427850fda47928fea2a644550c81c4c67
shubham_wadne <kaylekashubham@gmail.com>
2023-08-03 09:40:22 +0530

    solving error

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 
 const queryClient = new QueryClient();
 
+export const dynamic = 'force-dynamic'
 
 const inter = Inter({ subsets: ['latin'] })
 
